43fe255637b3b15fc28eb0442829b18bcdbc6f67
Elena.Koshelko <elenpulya@yandex.ru>
2026-09-13 14:04:26 +0700

    замечания ревьюера: про константы

--- src/main/java/pageobject/MainPage.java
+++ src/main/java/pageobject/MainPage.java
@@ -13,11 +13,11 @@ public class MainPage {
     public static final String PAGE_URL = "https://qa-scooter.praktikum-services.ru/";
     // локаторы элементов
     // Заголовок страницы
-    private final By LogoYandex = By.xpath("//img[@alt='Yandex']");
+    private final By logoYandex = By.xpath("//img[@alt='Yandex']");
     // Заголовок страницы самокат
-    private final By LogoScooter = By.xpath("//img[@alt='Scooter']");
+    private final By logoScooter = By.xpath("//img[@alt='Scooter']");
     // Кнопка "Заказать" в шапке страницы
-    private final By HeaderOrderButton = By.className("Button_Button__ra12g");
+    private final By headerOrderButton = By.className("Button_Button__ra12g");
     // Кнопка "Статус заказа" в шапке страницы
     private final By orderStatusButton = By.className("Header_Link__1TAG7");
     // Кнопка "Заказать" в середине страницы
@@ -25,29 +25,29 @@ public class MainPage {
     // Заголовок раздела "Вопросы о важном"
     private final By question = By.className("Home_SubHeader__zwi_E");
     // Константы для вопросов о важном УБРАТЬ ИХ В ТЕСТ
-    public static final String EXPECTED_QUESTION_1 = "Сколько это стоит? И как оплатить?";
-    public static final String EXPECTED_ANSWER_1 = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+    public static final String EXPECTED_QUESTION_ONE = "Сколько это стоит? И как оплатить?";
+    public static final String EXPECTED_ANSWER_ONE = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
 
-    public static final String EXPECTED_QUESTION_2 = "Хочу сразу несколько самокатов! Так можно?";
-    public static final String EXPECTED_ANSWER_2 = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
+    public static final String EXPECTED_QUESTION_TWO = "Хочу сразу несколько самокатов! Так можно?";
+    public static final String EXPECTED_ANSWER_TWO = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
 
-    public static final String EXPECTED_QUESTION_3 = "Как рассчитывается время аренды?";
-    public static final String EXPECTED_ANSWER_3 = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
+    public static final String EXPECTED_QUESTION_THREE = "Как рассчитывается время аренды?";
+    public static final String EXPECTED_ANSWER_THREE = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
 
-    public static final String EXPECTED_QUESTION_4 = "Можно ли заказать самокат прямо на сегодня?";
-    public static final String EXPECTED_ANSWER_4 = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
+    public static final String EXPECTED_QUESTION_FOUR = "Можно ли заказать самокат прямо на сегодня?";
+    public static final String EXPECTED_ANSWER_FOUR = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
 
-    public static final String EXPECTED_QUESTION_5 = "Можно ли продлить заказ или вернуть самокат раньше?";
-    public static final String EXPECTED_ANSWER_5 = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
+    public static final String EXPECTED_QUESTION_FIVE = "Можно ли продлить заказ или вернуть самокат раньше?";
+    public static final String EXPECTED_ANSWER_FIVE = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
 
-    public static final String EXPECTED_QUESTION_6 = "Вы привозите зарядку вместе с самокатом?";
-    public static final String EXPECTED_ANSWER_6 = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
+    public static final String EXPECTED_QUESTION_SIX = "Вы привозите зарядку вместе с самокатом?";
+    public static final String EXPECTED_ANSWER_SIX = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
 
-    public static final String EXPECTED_QUESTION_7 = "Можно ли отменить заказ?";
-    public static final String EXPECTED_ANSWER_7 = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
+    public static final String EXPECTED_QUESTION_SEVEN = "Можно ли отменить заказ?";
+    public static final String EXPECTED_ANSWER_SEVEN = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
 
-    public static final String EXPECTED_QUESTION_8 = "Я жизу за МКАДом, привезёте?";
-    public static final String EXPECTED_ANSWER_8 = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
+    public static final String EXPECTED_QUESTION_EIGHT = "Я жизу за МКАДом, привезёте?";
+    public static final String EXPECTED_ANSWER_EIGHT = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
     // Поиск кнопки вопроса по тексту
     private static final String BUTTON_BY_TEXT_XPATH =
         "//div[contains(@class, 'accordion__button') and contains(text(), '%s')]";
@@ -76,7 +76,7 @@ public class MainPage {
     }
         // Кликнуть по верхней кнопке "Заказать"
     public void clickHeaderOrderButton() {
-        driver.findElement(HeaderOrderButton).click();
+        driver.findElement(headerOrderButton).click();
     }
     // Проскроллить до нижней кнопки "Заказать" и кликнуть по ней
     public void clickFooterOrderButton() {
